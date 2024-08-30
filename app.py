048dd67f4aa93f091c426cc057d0c4dfad396181
Bruno Moreira <149931076+martechbm@users.noreply.github.com>
2024-08-30 17:54:08 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import qrcode
-import io
-import base64
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cardapio.db'
-db = SQLAlchemy(app)
